b19d02dcada57a0ea29c9564c1aa0bcc87d96cca
Jef Malfliet <jef.malfliet@student.hogent.be>
2019-11-15 15:17:56 +0100

    [FEAT] add xml for activity and friend

--- app/src/main/java/com/example/hangaround/MainActivity.kt
+++ app/src/main/java/com/example/hangaround/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.hangaround
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.hangaround.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
     }
 }
